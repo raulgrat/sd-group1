907e67e648e19e3d6a8d2f33ce9011b0a167924d
raulgrat <ra379063@ucf.edu>
2025-02-26 01:50:44 -0500

    34th commit (LED connection)

--- routes/unlimitedRoutes.js
+++ routes/unlimitedRoutes.js
@@ -10,7 +10,4 @@ router.post("/collectSensorData", collectSensorData);
 router.post("/ledcontrol", updateLEDStates);
 router.post("/updateScore", updateScore);
 
-// Serve static files from the React app
-app.use(express.static('client/build'));
-
 export default router;
