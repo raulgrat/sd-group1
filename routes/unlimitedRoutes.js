ab77b4dc38e124ecb328595d71fec13fed1ae691
raulgrat <ra379063@ucf.edu>
2025-02-26 01:48:50 -0500

    33th commit (LED connection)

--- routes/unlimitedRoutes.js
+++ routes/unlimitedRoutes.js
@@ -13,10 +13,4 @@ router.post("/updateScore", updateScore);
 // Serve static files from the React app
 app.use(express.static('client/build'));
 
-// Catch-all handler for any requests that don't match the above
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'));
-  });
-
-
 export default router;
